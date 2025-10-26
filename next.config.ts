680f9496c4ebee954835c6ece5c9133ac26bf4a7
Susana Chang <samanta.chang@utec.edu.pe>
2025-10-26 00:40:11 -0700

    url rewrite fix

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
 
         destination: 'http://localhost:5000/api/upload-audio',
       },
+      {
+        source: '/api/upload-image',
+        destination: 'http://localhost:5000/api/upload-image',
+      },
     ]
   },
 };
